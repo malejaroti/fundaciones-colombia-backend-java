81ab5bd5464129dc003a0264ba86c4edc0d92599
Alejandra <malejart@gmail.com>
2026-06-04 17:07:03 +0200

    fix: Update CORS configuration to allow requests from Netlify domain

--- src/main/java/com/fundacionescolombia/backend/config/CorsConfig.java
+++ src/main/java/com/fundacionescolombia/backend/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173, https://fundaciones-colombia.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
